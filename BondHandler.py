2fb4d0724eef05efecca68393ae54644a1296bee
starfruitinc@gmail.com <starfruitinc@gmail.com>
2016-01-23 14:35:32 -0500

    Fix if

--- BondHandler.py
+++ BondHandler.py
@@ -55,9 +55,9 @@ class BondHandler:
             if order[0]==1000:
                 if self.currentPos+order[1] >90:
                     self.sendOrder(True,order[0],1000)
-        if self.counter%10==0:
-            self.sendOrder(False,1,1001)
-            self.sendOrder(True,1,999)
+	print (book[-1])
+        self.sendOrder(False,1,1001)
+        self.sendOrder(True,1,999)
 
     def fillOrder(self, obj): #stock is of type STOCK
         vol=int(obj['size'])
