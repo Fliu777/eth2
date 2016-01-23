b8b472c0f9108bd81aa4881474b90a20fa83f2aa
Zihao Zhang <zzh8829@gmail.com>
2016-01-23 12:16:05 -0500

    hi

--- test.py
+++ test.py
@@ -1,27 +1,48 @@
 import socket 
+import sys 
+import timer
 
-IP = socket.gethostbyname('zihao.me')
-#IP = '127.0.0.1'
-PORT = 3000
+#IP = socket.gethostbyname('test-exch-janeavenue')
+IP = socket.gethostbyname('production')
+PORT = 25000
 
-class Socket:
+TEAMNAME = 'JANEAVANUE'
+
+class Client:
   BUFFER_SIZE = 1024
   
   def __init__(self, ip, port):
     self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.socket.connect((ip, port))
+    self.file = self.socket.makefile('w+', 1)
 
   def send(self, message):
-    self.socket.send(message)
-    data = self.socket.recv(Socket.BUFFER_SIZE)
-    return data
+    print(message, file=self.file)
 
   def close(self):
     self.socket.close()
 
-s = Socket(IP, PORT)
+  def update(self):
+    while self.file.readline().strip():
+
+
+
+s = Client(IP, PORT)
 
 print(s.send("hello\n"))
 
 s.close()
+
+
+if __name__ == '__main__':
+
+  c = Client()
+
+
+  while True:
+    c.update()
+
+    timer.sleep(0.05)
+
+
 
