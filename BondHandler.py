065694059b8779496fbcb0a0045b07ae472d5868
frank <franksliu12@gmail.com>
2016-01-23 11:37:10 -0500

    updates

---
+++ BondHandler.py
@@ -0,0 +1,8 @@
+class BondHandler:
+    def __init__(self):
+        pass
+    def sendOrder(self, connection):
+        #send order to buy at 10001
+        #send order to sell at 999
+    def fillOrder(self, Stock):
+        pass
---
+++ Stock.py
@@ -0,0 +1,6 @@
+class Stock
+    def __init__(self, symbol, amount, volume):
+        self.symbol=symbol
+        self.amount=amount
+        self.volume=volume
+        pass
