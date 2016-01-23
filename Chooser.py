40d46df057152879120e46de247ed7adff13ce5d
frank <franksliu12@gmail.com>
2016-01-23 11:21:20 -0500

    abstracts

--- Chooser.py
+++ Chooser.py
@@ -4,15 +4,15 @@ class Chooser:
 
     def __init__(self):
         pass
-    def loadAlgo(algo):
-        algorithms.append(algo)
-    def executeTrade():
+    def loadAlgo(self, algo):
+        Chooser.algorithms.append(algo)
+    def executeTrade(self):
         bestAlgo=None
         bestHistory=0
-        for algo in algorithms:
+        for algo in Chooser.algorithms:
             cur=algo.recent_history()
             if cur>bestHistory:
                 bestAlg=algo
                 bestHistory=cur
         if bestAlgo:
-            bestAlgo.next_trade(activeTrades)
+            bestAlgo.next_trade(Chooser.activeTrades)
