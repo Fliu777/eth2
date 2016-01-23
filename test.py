169e29a7581bac917e8dbd2af92926f9102cb3a4
starfruitinc@gmail.com <starfruitinc@gmail.com>
2016-01-23 15:35:00 -0500

    Add traceback

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import sys
 import time
 import json
 from time import gmtime, strftime
+import traceback
 
 import Client
 
@@ -44,7 +45,7 @@ def run(server, PORT):
 
           vc.feed(obj)
       except Exception as e:
-          print ("An error occured {}".format(e.message))
+	  traceback.print_exc()
 
     if obj['type'] == 'open':
       if 'BOND' in obj['symbols']:
