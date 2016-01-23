58e5cd4188b0acc158a52798f941ed1f039d33cd
Ubuntu <ubuntu@bot-janeavenue.eth1-waterloo.js-eth>
2016-01-23 17:46:30 +0000

    test

--- BondHandler.py
+++ BondHandler.py
@@ -6,26 +6,26 @@ class BondHandler:
     counter=0
     def __init__(self, connection):
         BondHandler.connection=connection
-        self.sendOrder(True,100,999)
-        self.sendOrder(False, 100,1001)
+        self.sendOrder(True, 1,999)
+        self.sendOrder(False, 1,1001)
         pass
-    def sendOrder(self, isBuy, amount, price):
+    def sendOrder(self, isBuy, price, amount):
         if isBuy:
             BondHandler.counter+=1
-            connection.send("ADD "+str(BondHandler.counter)+" BOND BUY "+str(amount)+" "+str(price))
+            BondHandler.connection.send("ADD "+str(BondHandler.counter)+" BOND BUY "+str(amount)+" "+str(price))
         else:
             BondHandler.counter+=1
-            connection.send("ADD "+str(BondHandler.counter)+" BOND SELL "+str(amount)+" "+str(price))
+            BondHandler.connection.send("ADD "+str(BondHandler.counter)+" BOND SELL "+str(amount)+" "+str(price))
         pass
     def updatePrice(self, stock, buyPrices=[], sellPrices=[]):
         if BondHandler.buyOrder:
-            self.sendOrder(True,100,999)
+            self.sendOrder(True,1,999)
             #buyOrder=False
         if BondHandler.buyOrder and len(sellPrices)>0 and sellPrices[0][0]>=1000:
             #sell at 1000, which is fair value
-            self.sendOrder(False,100,sellPrices[0][0])
+            self.sendOrder(False,1,sellPrices[0][0])
         elif BondHandler.sellOrder:
-            self.sendOrder(False,100,1001)
+            self.sendOrder(False,1,1001)
 
     def fillOrder(self, stock, orderbook): #stock is of type STOCK
         if stock.volume>0:
--- test.py
+++ test.py
@@ -1,10 +1,11 @@
-import socket 
-import sys 
-import timer
 from __future__ import print_function
+import socket
+import sys
+import time
 
 IP = socket.gethostbyname('test-exch-janeavenue')
 #IP = socket.gethostbyname('production')
+#IP = "1.1.1.1"
 PORT = 20000
 TEAMNAME = 'JANEAVANUE'
 
@@ -17,18 +18,19 @@ class Client:
     self.file = self.socket.makefile('w+', 1)
 
   def send(self, message):
+    print(message)
     print(message, file=self.file)
 
   def close(self):
     self.socket.close()
 
   def read(self):
-    return self.file.readline().strip()
-
+    data = self.file.readline().strip() 
+    print(data)
 
 client = Client(IP, PORT)
 
-client.send("HELLO JANEAVANUE")
+client.send("HELLO JANEAVENUE\n")
 
 import BondHandler
 
@@ -39,5 +41,5 @@ while True:
 
   bh.updatePrice(None)
 
-  timer.sleep(0.1)
+  time.sleep(0.1)
 
